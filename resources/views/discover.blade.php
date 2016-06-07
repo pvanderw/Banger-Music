a072a333631eb4e86fc8cf66f29ca2402c6fe690
pvanderw <pvanderw@usc.edu>
2016-06-07 15:09:15 -0700

    autoplay

--- resources/views/discover.blade.php
+++ resources/views/discover.blade.php
@@ -88,7 +88,7 @@
 					height="300"
 					scrolling="no"
 					frameborder="no"
-					src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/{{$tracks[$counter]->id}}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true">
+					src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/{{$tracks[$counter]->id}}&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true">
 				</iframe>
 			</div>
 			<?php if ($counter < $trackCount-1) : ?>
